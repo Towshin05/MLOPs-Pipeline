71802d0ff007053c2fe4a08abedeb68ccbc25563
Towshin05 <u2004086@student.cuet.ac.bd>
2026-05-30 12:16:44 +0600

    notebook added

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    author="Towshin Hossain",
-    author_email="towshinhossain2@gmail.com",
+    # author="Towshin Hossain",
+    # author_email="towshinhossain2@gmail.com",
     packages=find_packages()
 )
 
